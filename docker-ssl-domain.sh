6d96db1f8339cb1ba45b3bb20a8ea4d12fc96b08
alireza ghavabesh <jacobmarcosoliver@gmail.com>
2025-09-02 17:17:24 +0330

    improved domain ssl setup

--- docker-ssl-domain.sh
+++ docker-ssl-domain.sh
@@ -330,7 +330,7 @@ if [ "$SSL" = "true" ]; then
 server {
     listen 80;
     server_name _;
-    return 301 https://$host$request_uri;
+    return 301 https://\$host\$request_uri;
 }
 
 server {
@@ -348,14 +348,14 @@ server {
     location / {
         proxy_pass http://app:${PORT};
         proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection 'upgrade';
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-        proxy_set_header X-Forwarded-Host $host;
-        proxy_cache_bypass $http_upgrade;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header X-Forwarded-Host \$host;
+        proxy_cache_bypass \$http_upgrade;
         proxy_read_timeout 86400;
     }
 }
